4ed1a5478f6b36be9aa79d1f312cd4a5d43525db
Ability Ekpat <abilityekpat@gmail.com>
2023-06-11 20:49:00 +0100

    added firebase to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:taskly/screens/authentication/welcome_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:taskly/screens/home_screens/add_task.dart';
 import 'package:taskly/screens/home_screens/home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
